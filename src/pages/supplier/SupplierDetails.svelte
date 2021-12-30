302e18e66834cf281267cacf330a3d69700e315e
soso-collab <zouhisof@students.zhaw.ch>
2021-12-30 18:08:19 +0100

    supplierdetail.svelte delete funktion eingefügt

--- src/pages/supplier/SupplierDetails.svelte
+++ src/pages/supplier/SupplierDetails.svelte
@@ -19,14 +19,22 @@
                 console.log(supplier)
             });
     }
+
+    function deleteSupplier(){
+        console.log(supplierId);
+        axios
+        .delete("http://localhost:8080/supplier/" + supplierId)
+        .then((response)=> {
+            alert("Supplier is deleted");
+        });
+    }
+
 </script>
 
 <div class="mb-5">
     <h1 class="mt-3">Supplier (ID: {supplierId})</h1>
     <p>Name: {supplier.name}</p>
-    <p>Birthdate: </p>
 
-    
 </div>
 
 
